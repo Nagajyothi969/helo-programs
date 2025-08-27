325e5d1455771908beb828f014d4057f242b5acc
Nagajyothi969 <nagajyothigurrala905@gmail.com>
2025-08-27 17:53:53 +0530

    second class program

---
+++ src/hloworld/hlowolrd2.java
@@ -0,0 +1,12 @@
+package hloworld;
+
+public class hlowolrd2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 15;
+		int c = a+b;
+		System.out.println("sum of the a& b"+" " + c );
+	}
+
+}
